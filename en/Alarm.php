58bfd315958a477cb73e7edd9ef3c6754d351e85
Aghla Taoufık <Ag.Taoufk@gmail.com>
2021-12-14 09:44:10 +0300

    remove unnecessary files

--- en/Alarm.php
+++
@@ -1,9 +0,0 @@
-<?php 
- return array (
-  'type' => 'type',
-  'content' => 'content',
-  'device' => 'device',
-  'location' => 'location',
-  'user' => 'user',
-  'alarm_fired' => 'Alarm Fired',
-);
--- tr/Alarm.php
+++
@@ -1,9 +0,0 @@
-<?php 
- return array (
-  'type' => 'Tip',
-  'content' => 'İçerik',
-  'device' => 'Cihaz',
-  'location' => 'Lokasyon',
-  'user' => 'Kullanıcı',
-  'alarm_fired' => 'Alarm verildi.',
-);
