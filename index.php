e0184478a572bd1a4b0dff30160edf4b240cd9e0
Aghla Taoufık <Ag.Taoufk@gmail.com>
2021-12-15 15:23:15 +0300

    Add Validation/RndName/DeleteAfter

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Converter</title>
 </head>
-<body>
+<body style="text-align: center;">
     <form method="POST" action="upload.php" enctype="multipart/form-data">
         <div>
-            <span>Upload XLS/XLSX File:</span>
-            <input type="file" name="uploadedFile[]" accept=".xls, .xlsx/*" multiple="multiple"/>
-        </div>
-        <input type="submit" name="uploadBtn" value="Json" />
-        <input type="submit" name="uploadBtn" value="Array" />
+            <h1 style="color:rgb(117, 59, 17);">Upload XLS/XLSX File : </h1><br>
+            <input type="file" name="uploadedFile[]" accept=".xls, .xlsx/*" multiple="multiple" style="padding: 10px 20px; border: 2px solid rgb(117, 59, 17); border-radius: 10px;cursor: pointer"/>
+        </div><br>
+        <input type="submit" name="uploadBtn" value="Json" style="background-color: rgb(201, 115, 54); padding: 10px 50px; border: none; border-radius: 10px;cursor: pointer" />
+        <input type="submit" name="uploadBtn" value="Array" style="background-color: rgb(201, 115, 54); padding: 10px 50px; border: none; border-radius: 10px;cursor: pointer;"/>
     </form>
 </body>
 </html>
